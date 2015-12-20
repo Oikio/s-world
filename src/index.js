e7d8eb7a2f944c2f122fb3cbd8773aa4d913fd02
oikio <oikiostas@gmail.com>
2015-12-20 17:13:07 +0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+var r = require('ramda')
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+  // create a wrapper around native canvas element (with id="c")
+  var canvas = new fabric.Canvas('c');
+
+// create a rectangle object
+  var rect = new fabric.Rect({
+    left: 100,
+    top: 100,
+    fill: 'red',
+    width: 20,
+    height: 20
+  });
+
+// "add" rectangle onto canvas
+  canvas.add(rect);
+
+
+})
